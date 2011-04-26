357990ab1190da1e9ac407f8fa2c00e25e90b0de
Yukihiro Nomura <nomuray-tky@umin.ac.jp>
2011-04-26 16:49:50 +0900

    Add class for CAD feedback

---
+++ app/lib/CadFeedback.class.php
@@ -0,0 +1,27 @@
+<?php
+
+class CadFeedback
+{
+	public static function GetFeedbackID($pdo, $jobID, $feedbackMode, $userID)
+	{
+		$pdo = DBConnector::getConnection();
+		$ret = 0;
+
+		$sqlStr = "SELECT fb_id FROM feedback_list WHERE job_id=?";
+
+		if($feedbackMode == "personal")  $sqlStr .= " AND entered_by=? AND is_consensual='f'";
+		else                             $sqlStr .= " AND is_consensual='t'";
+
+		$stmt = $pdo->prepare($sqlStr);
+		$stmt->bindparam(1, $jobID);
+		if($feedbackMode == "personal")  $stmt->bindParam(2, $userID);
+		$stmt->execute();
+
+		if($stmt->rowCount() == 1)  $ret = $stmt->fetchColumn();
+
+		return $ret;
+	}
+
+}
+
+?>
