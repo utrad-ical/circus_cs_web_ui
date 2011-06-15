a5d8e565eb34a0eff8b38b53b643cefc194fd615
Soichiro Miki <smiki-tky@umin.ac.jp>
2011-06-15 14:27:02 +0900

    Always assign $currentUser variable in SmartyEx

--- app/lib/SmartyEx.class.php
+++ app/lib/SmartyEx.class.php
@@ -11,20 +11,20 @@ class SmartyEx extends Smarty
 {
 	/**
 	 * Constructor.
-	 * Initializes directories and several modifiers.
+	 * Initializes directories.
 	 */
 	public function __construct()
 	{
 		global $BASE_DIR, $DIR_SEPARATOR, $WEB_UI_ROOT;
-		parent::__construct(); // initalization of parent class
-
+		parent::__construct();
 		$rootPath = $WEB_UI_ROOT . $DIR_SEPARATOR . 'app' . $DIR_SEPARATOR . 'smarty' . $DIR_SEPARATOR;
-
 		$this->template_dir  = $rootPath . 'templates/';
 		$this->compile_dir   = $rootPath . 'templates_c/';
 		$this->config_dir    = $rootPath . 'configs/';
 		$this->cache_dir     = $rootPath . 'cache/';
 		$this->plugins_dir[] = $rootPath . 'plugins/';
+
+		$this->assign('currentUser', Auth::currentUser());
 	}
 }
 
