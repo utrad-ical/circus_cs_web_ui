360108746f82b32cc05a9394f46951f25c0aed66
Yukihiro Ohno <y-ohno@j-mac.co.jp>
2011-06-22 19:42:01 +0900

    WebAPI: implement login action.

--- app/lib/api/LoginAction.class.php
+++ app/lib/api/LoginAction.class.php
@@ -2,8 +2,67 @@
 
 class LoginAction extends ApiAction
 {
+	protected static $required_privileges = array(
+		Auth::API_EXEC
+	);
+	
+	
+	function requiredPrivileges()
+	{
+		return self::$required_privileges;
+	}
+	
+	
 	function execute($api_request)
 	{
+		$params = $api_request['params'];
+		$action = $api_request['action'];
+		$mode = $params['mode'];
+		
+		$user = ApiExec::currentUser();
+		if(!isset($user)) {
+			throw new ApiException('Authentication required');
+		}
+		
+		$result = null;
+		switch ($mode)
+		{
+			case "getOnetime":
+				$userid = $user->user_id;
+				$time = date("Y-m-d H:i:s");
+				$ip = getenv("REMOTE_ADDR");
+				
+				// Delete old onetime password
+				$sqlStr = "DELETE FROM user_onetime"
+						. " WHERE user_id = ? "
+						. " OR registered_at < ?";
+				DBConnector::query(
+					$sqlStr,
+					array($userid, date("Y-m-d H:i:s", time()-60))
+				);
+				
+				// Generate one-time password
+				$onetime = md5(uniqid().mt_rand());
+				
+				// Register one-time password
+				$sqlStr = "INSERT INTO user_onetime"
+						. " (user_id, ip_address, onetime_pass, registered_at)"
+						. " VALUES (?, ?, ?, ?)";
+				DBConnector::query(
+					$sqlStr,
+					array($userid, $ip, $onetime, $time)
+				);
+				
+				$result = array("onetime" => $onetime);
+				break;
+				
+			case "newSession":
+			default:
+				session_start();
+				Auth::createSession($user);
+				break;
+		}
+		
 		$res = new ApiResponse();
 		$res->setResult($action, $result);
 		return $res;
---
+++ pub/login.php
@@ -0,0 +1,51 @@
+<?php
+
+include("common.php");
+$toTopDir = './';
+
+try
+{
+	$onetime = $_GET['onetime'];
+	$jump = $_GET['jump'];
+	
+	// Get valid onetime pass
+	$sqlStr = 'SELECT *'
+			. ' FROM user_onetime'
+			. ' WHERE onetime_pass = ?'
+			. ' AND registered_at > ?';
+	
+	$result = DBConnector::query(
+		$sqlStr,
+		array($onetime, date("Y-m-d H:i:s", time()-60)),
+		'ARRAY_ASSOC'
+	);
+	
+	// User authentication
+	$user = new User($result['user_id']);
+	if(!$user || !$user->enabled) {
+		throw new Exception('Authentication failed.');
+	}
+	
+	// Delete used onetime password
+	$sqlStr = "DELETE FROM user_onetime"
+			. " WHERE onetime_pass = ? ";
+	DBConnector::query(
+		$sqlStr,
+		array($onetime)
+	);
+	
+	session_start();
+	Auth::createSession($user);
+	
+	// Redirect
+	header('location: ' . $toTopDir . $jump);
+}
+catch (Exception $e)
+{
+	$smarty = new SmartyEx();
+	$smarty->assign('message', $e->getMessage());
+	$smarty->display('critical_error.tpl');
+	exit;
+}
+
+?>
