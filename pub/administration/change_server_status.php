baecb57846c976fc952b65bca5863474112cc2f8
Yukihiro Nomura <nomuray-tky@umin.ac.jp>
2011-06-30 19:59:17 +0900

    Bug fix of change_server_status.php
    Remove include("server_status_private.php") in change_server_status.php
    
    Forgot to add reset_plugin_job.php

--- pub/administration/change_server_status.php
+++ pub/administration/change_server_status.php
@@ -2,8 +2,6 @@
 	include("../common.php");
 	Auth::checkSession();
 
-	include("server_status_private.php");
-
 	$mode = (isset($_REQUEST['mode']) && ($_SESSION['ticket'] == $_REQUEST['ticket'])) ? $_REQUEST['mode'] : "";
 	$serviceName = (isset($_REQUEST['serviceName'])) ? $_REQUEST['serviceName'] : "";
 
---
+++ pub/administration/reset_plugin_job.php
@@ -0,0 +1,59 @@
+<?php
+$params = array('toTopDir' => "../");
+include_once("../common.php");
+Auth::checkSession();
+Auth::purgeUnlessGranted(Auth::SERVER_OPERATION);
+
+$dstData = array('message' => "");
+
+$hostName = '127.0.0.1';
+$svStat = WinServiceControl::GetStatus($PLUGIN_JOB_MANAGER_SERVICE, $hostName);
+
+if($svStat['str'] != 'Stopped')
+{
+	$dstData['message'] = "[ERROR] Fail to reset plug-in job queue (" . $PLUGIN_JOB_MANAGER_SERVICE . 'is not stopped)';
+}
+
+if($dstData['message'] == "")
+{
+	try
+	{
+		$nowDateTime = date("Y-m-d H:i:s");
+
+		// Connect to SQL Server
+		$pdo = DBConnector::getConnection();
+
+		// Begin transaction
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$pdo->beginTransaction();	// Begin transaction
+
+		$sqlStr = "UPDATE job_queue SET status=1,"
+				. " pm_id=NULL, registered_at= :dt, updated_at= :dt"
+				. " WHERE status<=3";
+		$stmt = $pdo->prepare($sqlStr);
+		$stmt->bindValue(':dt', $nowDateTime);
+		$stmt->execute();
+
+		$sqlStr = "UPDATE executed_plugin_list SET status=1, executed_at=?"
+				. " WHERE status>=1 AND status<=3";
+		$stmt = $pdo->prepare($sqlStr);
+		$stmt->bindValue(1, $nowDateTime);
+		$stmt->execute();
+
+		// Commit transaction
+		$pdo->commit();
+
+		$dstData['message'] = 'Success to reset plug-in job queue. Please restart' . $PLUGIN_JOB_MANAGER_SERVICE;
+
+	}
+	catch (PDOException $e)
+	{
+		$pdo->rollBack();
+		$dstData['message'] = '[ERROR] Fail to reset plug-in job queue';
+		//$dstData['message'] = $e->getMessage();
+	}
+	$pdo = null;
+}
+
+echo json_encode($dstData);
+?>
