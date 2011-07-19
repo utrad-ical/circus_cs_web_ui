66d05b02ab8b4190f4b3cce1a7be2fb880e69e8a
Soichiro Miki <smiki-tky@umin.ac.jp>
2011-07-19 12:09:08 +0900

    QueryFeedbackAction: correctly ignores temporary feedback set

--- app/lib/api/QueryFeedbackAction.class.php
+++ app/lib/api/QueryFeedbackAction.class.php
@@ -11,32 +11,32 @@ class QueryFeedbackAction extends ApiAction
 	{
 		return self::$required_privileges;
 	}
-	
-	
+
+
 	function execute($api_request)
 	{
 		$params = $api_request['params'];
 		$action = $api_request['action'];
-	
+
 		if($this->check_params($params) == FALSE)
 		{
 			throw new ApiException("Invalid parameter.", ApiResponse::STATUS_ERR_OPE);
 		}
-		
+
 		$jobID  = $params['jobID'];
 		$kind   = $params['kind'];
 		if (!isset($kind)) {
 			$kind = "all";
 		}
 		$userID = $params['userID'];
-		
+
 		// Retrieve the CAD Result
 		$cadResult = new CadResult($jobID);
 		if (!isset($cadResult->job_id))
 		{
 			$result = null;
 		}
-		$feedback = $cadResult->queryFeedback($kind, $userID, false);
+		$feedback = $cadResult->queryFeedback($kind, $userID);
 
 		$result = array();
 		foreach ($feedback as $f) {
@@ -49,34 +49,34 @@ class QueryFeedbackAction extends ApiAction
 				)
 			);
 		}
-		
+
 		if (count($result) <= 0)
 		{
 			$res = new ApiResponse();
 			$res->setResult($action, null);
 			return $res;
 		}
-		
+
 		$res = new ApiResponse();
 		$res->setResult($action, $result);
 		return $res;
 	}
-	
-	
+
+
 	private function check_params($params)
 	{
 		if(!isset($params['jobID'])) {
 			return false;
 		}
-		
+
 		$kind = $params['kind'];
 		if (isset($kind) && !in_array($kind, array("all", "personal", "consensual", "user"))) {
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 }
 
 ?>
