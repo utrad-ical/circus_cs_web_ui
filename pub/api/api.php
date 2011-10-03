01e2b3f56772803bb56bc5365f2da186f664bc18
Soichiro Miki <smiki-tky@umin.ac.jp>
2011-10-04 01:30:55 +0900

    Web API: allow standard 'urlencoded' post values.
    Web API can additionally understand
    standard POST style request
    such as 'action=foo&a=b&c=10'.

--- pub/api/api.php
+++ pub/api/api.php
@@ -6,13 +6,14 @@ if (is_null($api_request))
 {
 	$api_request = json_decode(file_get_contents("php://input"), true);
 }
+if (is_null($api_request) && $_POST['action'])
+	$api_request = $_POST;
 
 if (is_null($api_request))
 {
 	$res = new ApiResponse();
 	$res->setError(NULL, ApiResponse::STATUS_ERR_OPE, "Request format is invalid.");
 	echo $res->getJson();
-	
 	exit;
 }
 else
@@ -24,9 +25,7 @@ else
 	catch (Exception $e)
 	{
 		$action = $api_request['action'];
-		
 		$res = new ApiResponse();
-//		$res->setError($action, ApiResponse::STATUS_ERR_SYS, "Internal system error." . $e->getMessage());
 		$res->setError($action, ApiResponse::STATUS_ERR_SYS, "Internal system error.");
 		echo $res->getJson();
 	}
