7ea6ee7bfb8ce539d6bb6d34b8932c7e6df8600e
Soichiro Miki <smiki-tky@umin.ac.jp>
2011-10-09 22:14:53 +0900

    Fix: accidental deletion of required scrpit
    Brought back 'register_feedback.php' deleted
    in commit:14cfb4f9
    Delete actual legacy script 'feedback_registration.php'.

---
+++ pub/cad_results/register_feedback.php
@@ -0,0 +1,118 @@
+<?php
+
+include("../common.php");
+Auth::checkSession(false);
+
+//------------------------------------------------------------------------------
+// Import $_POST variables and validation
+//------------------------------------------------------------------------------
+
+$validator = new FormValidator();
+$validator->addRules(array(
+	'jobID' => array(
+		'type' => 'int',
+		'required' => true,
+		'min' => 1,
+	),
+	'temporary' => array(
+		'type' => 'select',
+		'required' => false,
+		'options' => array(1, 0),
+		'default' => 0
+	),
+	'feedbackMode' => array(
+		'type' => 'select',
+		'required' => true,
+		'options' => array('personal', 'consensual')
+	),
+	'feedback' => array(
+		'type' => 'json',
+		'required' => true
+	)
+));
+
+try {
+	if($validator->validate($_POST))
+		$params = $validator->output;
+	else
+		throw new Exception(implode("\n", $validator->errors));
+	$is_consensual = $params['feedbackMode'] == 'consensual';
+
+	$dstData = array();
+	$job_id = $params['jobID'];
+
+	$currentUser = Auth::currentUser();
+
+	$cadResult = new CadResult($job_id);
+	if (!$cadResult->job_id)
+		throw new Exception('No CAD result found for the specified Job ID.');
+	$reason = 'You can not enter the feedback for unknown reason.';
+	if ($is_consensual)
+		$av = $cadResult->feedbackAvailability('consensual', $currentUser, $reason);
+	else
+		$av = $cadResult->feedbackAvailability('personal', $currentUser, $reason);
+	if ($av != 'normal')
+		throw new Exception($reason);
+
+	if (registerFeedback($job_id, $params['feedback'], $params['temporary'], $is_consensual) === true)
+		echo json_encode(array('status' => 'OK'));
+	else
+		throw new Exception('Failed!');
+} catch (Exception $e) {
+	echo json_encode(array(
+		'action' => 'registerFeedback',
+		'status' => 'SysError',
+		'error' => array(
+			'message' => $e->getMessage()
+		)
+	));
+}
+exit;
+
+
+//------------------------------------------------------------------------------
+
+function registerFeedback($job_id, $feedback, $temporary, $is_consensual)
+{
+	$pdo = DBConnector::getConnection();
+	$user = Auth::currentUser();
+	$user_id = $user->user_id;
+
+	// 'demo' as magic group: No actual feedback registration performed!
+	$groups = $user->Group;
+	foreach ($groups as $grp)
+		if ($grp->group_id == 'demo')
+			return true;
+
+	if (strlen($user_id) == 0)
+		throw new Exception('Session not established.');
+
+	// Delete existing feedback set (temporary or not)
+	$dummy = new Feedback();
+	$cond = array(
+		'is_consensual' => $is_consensual ? 'TRUE' : 'FALSE',
+		'job_id' => $job_id
+	);
+	if (!$is_consensual)
+		$cond['entered_by'] = $user_id; // only delete my personal feedback
+	$fbs = $dummy->find($cond);
+	foreach ($fbs as $delete_fb)
+		Feedback::delete($delete_fb->fb_id);
+
+	// Save the new feedback data
+	$fb = new Feedback();
+	$fb->save(array(
+		'Feedback' => array(
+			'job_id' => $job_id,
+			'is_consensual' => $is_consensual ? 'TRUE' : 'FALSE',
+			'status' => $temporary ? Feedback::TEMPORARY : Feedback::REGISTERED,
+			'entered_by' => $user_id,
+			'registered_at' => date('Y-m-d H:i:s')
+		),
+		'blockFeedback' => $feedback['blockFeedback'],
+		'additionalFeedback' => $feedback['additionalFeedback']
+	));
+	return true;
+}
+
+?>
