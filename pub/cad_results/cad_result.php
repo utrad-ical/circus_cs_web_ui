801d4e34a8ea63007bbc1db8a3d3deb392838160
Soichiro Miki <smiki-tky@umin.ac.jp>
2012-04-26 23:06:00 +0900

    Use constant for Job status

--- pub/cad_results/cad_result.php
+++ pub/cad_results/cad_result.php
@@ -54,9 +54,9 @@ function show_cad_results($jobID, $feedbackMode) {
 	$cadResult = new CadResult($jobID);
 	if (!isset($cadResult->job_id))
 		critical_error('The CAD result for this ID was not found.', 'Not Found');
-	if ($cadResult->status == -1)
+	if ($cadResult->status == Job::JOB_FAILED)
 		critical_error('This CAD job did not finish correctly.', 'Execution Error');
-	if ($cadResult->status != 4)
+	if ($cadResult->status != Job::JOB_SUCCEEDED)
 		critical_error('This CAD job has not finished yet.', 'Not Finished');
 	set_include_path(get_include_path() . PATH_SEPARATOR . $cadResult->Plugin->configurationPath());
 
