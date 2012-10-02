utrad-ical/circus_cs_web_ui

commit a2908f913394dafbc4478ecc7720931e4f306755

Fix: wrong returned value from CadResult::webPathOfPluginPub()
fixes #166

app/lib/models/CadResult.class.php
@@ -37,6 +37,8 @@ class CadResult extends Model
 	protected $rawResult;
 	protected $presentation;
 
+	private $_cachedRelativeTop;
+
 	/**
 	 * Retrieves the list of feedback data associated with this CAD Result.
 	 * @param string $kind One of the following. 'personal', the list of
@@ -311,7 +313,9 @@ class CadResult extends Model
 	public function webPathOfPluginPub()
 	{
 		$plugin_name = $this->Plugin->fullName();
-		return "plugin/$plugin_name";
+		$r = $this->_cachedRelativeTop !== null ? $this->_cachedRelativeTop : relativeTopDir();
+		$this->_cachedRelativeTop = $r;
+		return $r . "plugin/$plugin_name";
 	}
 
 	/**
@@ -332,9 +336,10 @@ class CadResult extends Model
 	 */
 	public function webPathOfCadResult()
 	{
-		global $DIR_SEPARATOR_WEB, $SUBDIR_CAD_RESULT;
+		$r = $this->_cachedRelativeTop !== null ? $this->_cachedRelativeTop : relativeTopDir();
+		$this->_cachedRelativeTop = $r;
 		$str_id = $this->storage_id;
-		return '../storage/' . $str_id . '/' . $this->job_id; // TODO: change
+		return $r . 'storage/' . $str_id . '/' . $this->job_id;
 	}
 
 	/**

app/smarty/templates/cad_results/inspector_basic.tpl
@@ -1,4 +1,5 @@
 <h2>Basic</h2>
+{$cadResult->webPathOfPluginPub()|escape}
 <table class="col-tbl">
   <thead>
     <tr><th>Key</th><th>Value</th></tr>

commit 379641ac9b1a5a1293d8866db1e51a5ae9f98e88

Fix: Auto-including of 'cad_result.{js/css}' did not work
Regression caused by commit:a2908f91 .

closes #166

app/lib/models/CadResult.class.php
@@ -309,13 +309,20 @@ class CadResult extends Model
 
 	/**
 	 * Returns the URL of plugin-specific public directory.
+	 * @param bool $from_root If false, result path is relative to current
+	 * directory. If true, result is from CIRCUS CS web root.
 	 */
-	public function webPathOfPluginPub()
+	public function webPathOfPluginPub($from_root = false)
 	{
 		$plugin_name = $this->Plugin->fullName();
-		$r = $this->_cachedRelativeTop !== null ? $this->_cachedRelativeTop : relativeTopDir();
-		$this->_cachedRelativeTop = $r;
-		return $r . "plugin/$plugin_name";
+		if ($from_root)
+		{
+			return "plugin/$plugin_name";
+		} else {
+			$r = $this->_cachedRelativeTop !== null ? $this->_cachedRelativeTop : relativeTopDir();
+			$this->_cachedRelativeTop = $r;
+			return $r . "plugin/$plugin_name";
+		}
 	}
 
 	/**

pub/cad_results/cad_result.php
@@ -181,7 +181,7 @@ function show_cad_results($jobID, $feedbackMode) {
 		'extensions' => $extParameters
 	);
 
-	$url_pub  = $cadResult->webPathOfPluginPub() . '/';
+	$url_pub  = $cadResult->webPathOfPluginPub(true) . '/';
 	$path_pub = $cadResult->pathOfPluginPub() . '/';
 	if (file_exists($path_pub . 'cad_result.css'))
 	{