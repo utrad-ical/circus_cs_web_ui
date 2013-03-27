4bc5a58f5bf8d1011b57195933015364db291e85
Soichiro Miki <smiki-tky@umin.ac.jp>
2013-03-27 13:38:59 +0900

    Fix: display ID was not set when displayExtractType=all
    fixes: #188

--- app/lib/displaypresenter/DisplayPresenter.class.php
+++ app/lib/displaypresenter/DisplayPresenter.class.php
@@ -84,6 +84,7 @@ class DisplayPresenter extends CadBlockElement
 		switch ($this->params['displayExtractType'])
 		{
 			case 'all':
+				$input['display_id'] = 1;
 				$result = array(1 => $input);
 				break;
 			case 'row':
