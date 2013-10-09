dedaebaaefe0c80f6a553d216259025c535fe8b7
Soichiro Miki <smiki-tky@umin.ac.jp>
2013-10-10 01:09:37 +0900

    Rewrite "recent missed", using the new framework
    refs #47
    
    Still 'missed' feedback is hard-coded and needs to be configurable.

--- app/smarty/templates/home.tpl
+++ app/smarty/templates/home.tpl
@@ -1,67 +1,89 @@
+{capture name="require"}
+jq/ui/jquery-ui.min.js
+jq/ui/theme/jquery-ui.custom.css
+jq/jquery.mousewheel.js
+js/jquery.imageviewer.js
+{/capture}
 {capture name="extra"}
 {literal}
 <style type="text/css">
 #title {
 	background-color: #eee;
-	border: 1px solid #ccc;
+	border: 2px solid #ccc;
 	padding: 1em;
-	border-radius: 8px;
 	margin-bottom: 1em;
 }
-
 #content h1 {
 	background-color: transparent;
 	font-size: 20px;
 	height: auto;
-	text-shadow: 0 0 4px white;
+	text-shadow: 1px 1px 2px silver, 0 0 0.5em white;
 }
-
-#side {
-	width: 400px;
-	float: right;
-}
-
-#center {
-	width: 580px;
-	background-color: #888;
-}
-
-.module {
-	margin-bottom: 10px;
-	margin-left: 10px;
-}
-
-.module h2 {
+#content #side { width: 400px; float: right; }
+#content #center { width: 580px; }
+#content .module { margin-bottom: 10px;	margin-left: 10px; }
+#content .module h2 {
 	margin: 0 0 10px -10px;
 	border-bottom: 1px solid black;
+	clear: none;
 }
-
-.plugin_execution ul{
-	margin-top: -3px;
+.missed_block {
+	display: inline-block;
+	width: 280px;
+	border: 1px solid gray;
 }
-
-
-.news li {
-	list-style:none;
+.missed_block .detail {
+	float: right;
+	margin: 2px 2px;
 }
 </style>
+
+<script type="text/javascript">
+$(function() {
+	$('.viewer').each(function() {
+		var _this = $(this);
+		var series_uid = _this.data('series-uid');
+		var crop = _this.data('crop');
+		var extra_opt = _this.data('opt');
+		var loc = _this.data('loc').split(',').map(function(i){return parseInt(i);});
+		var options = {
+			source: new DicomDynamicImageSource(series_uid, ''),
+			index: loc[2],
+			min: loc[2],
+			max: loc[2],
+			width: 280,
+			cropRect: crop,
+			markers: [{
+				location_x: loc[0],
+				location_y: loc[1],
+				location_z: loc[2]
+			}],
+			markerStyle: 'circle',
+			useSlider: false,
+			useLocationText: false,
+			useMarkerLabel: false
+		};
+		options = $.extend(options, extra_opt);
+		console.log(options);
+		_this.imageviewer(options);
+	});
+});
+</script>
+
 {/literal}
 {/capture}
-{include file="header.tpl" head_extra=$smarty.capture.extra body_class=home}
+{include file="header.tpl" require=$smarty.capture.require
+	head_extra=$smarty.capture.extra body_class=home}
 
 <div id="title">
 <h1 class="themeColor">Welcome to CIRCUS Clinical Server</h1>
-<p>User: {$currentUser->user_id|escape} (from {$smarty.server.REMOTE_ADDR})</p>
-<p>Last login: {$smarty.session.lastLogin|escape} (from {$smarty.session.lastIPAddr})</p>
+<p><strong>User:</strong> {$currentUser->user_id|escape} (from {$smarty.server.REMOTE_ADDR})</p>
+<p><strong>Last login:</strong> {$smarty.session.lastLogin|escape} (from {$smarty.session.lastIPAddr})</p>
 </div>
-
-{foreach from=$modules item=module}
-
-{/foreach}
 
 <div id="side">
 	<div class="module news">
-	<h2>News</h2>
+		<h2>News</h2>
 		<ul>
 			{foreach from=$plugins item=item}
 			<li><strong>{$item.plugin_name|escape}&nbsp;v.{$item.version|escape}</strong> was installed.&nbsp;({$item.install_dt|escape})</li>
@@ -70,34 +92,49 @@
 	</div>
 
 	<div class="module plugin_execution">
-	<h2>Plug-in Execution</h2>
-		<h4>Total of plug-in execution: {$executionNum|escape} (since {$oldestExecDate|escape})</h4>
-
-		{if $executionNum > 0}
-			[Top {$cadExecutionData|@count}]</p>
-			<ul>
-				{foreach from=$cadExecutionData item=item}
-					<li>{$item.plugin_name|escape}&nbsp;v.{$item.version|escape}: {$item.cnt|escape}</li>
-				{/foreach}
-			</ul>
+		<h2>Plug-in Execution</h2>
+		<h4>Total of plug-in execution: {$execStats.count|escape} (since {$execStats.first|escape})</h4>
+		{if $execStats.count > 0}
+		[Top {$cadExecutionData|@count}]</p>
+		<ul>
+			{foreach from=$cadExecutionData item=item}
+				<li>{$item.plugin_name|escape}&nbsp;v.{$item.version|escape}: {$item.cnt|escape}</li>
+			{/foreach}
+		</ul>
 		{/if}
 	</div>
 </div>
 
 <div id="center">
+	{if !is_null($topMessage)}
+	<div class="module top_message">
+		<h2>Message</h2>
+		{$topMessage}
+	</div>
+	{/if}
 
-<div id="top_message">
-{$topMessage}
+	{if count($recentMissed)}
+	<div class="module recent_missed">
+		<h2>Latest Results</h2>
+		{foreach from=$recentMissed item=item}
+			{assign var="job" value=$item.job}
+			{assign var="d" value=$item.display}
+			{assign var="pt" value=$job->Series[0]->Study->Patient}
+			<div class="missed_block">
+				<a class="detail form-btn" href="cad_results/cad_result.php?jobID={$job->job_id}">Detail</a>
+				<p><strong>Pt.:</strong> {$pt->patient_name|escape} ({$pt->patient_id|escape})</p>
+				<p><strong>St.:</strong> {$job->Series[0]->Study->study_date|escape}</p>
+				<p><strong>CAD:</strong> {$job->Plugin->plugin_name|escape} v.{$job->Plugin->version|escape}</p>
+				<div class="viewer"
+					data-loc="{$d.location_x},{$d.location_y},{$d.location_z}"
+					data-series-uid="{$job->Series[0]->series_instance_uid|escape}"
+					data-crop="{$item.crop|@json_encode|escape}"
+					data-opt="{$item.opt|@json_encode|escape}">
+				</div>
+			</div>
+		{/foreach}
+	</div>
+	{/if}
 </div>
-
-
-{if $smarty.session.personalFBFlg==1 && $smarty.session.latestResults!='none' && $latestHtml !=""}
-<h2>Latest Results</h2>
-<div style="margin-left: 15px; border: 1px solid red;">
-	{$latestHtml}
-</div>
-{/if}
-</div>
-
 
 {include file="footer.tpl"}
--- pub/home.php
+++ pub/home.php
@@ -2,12 +2,8 @@
 require_once('common.php');
 Auth::checkSession();
 
-$data = array();
-
 try
 {
-	$pdo = DBConnector::getConnection();
-
 	// News block
 	$plugins = array_map(
 		function($item) { return $item->getData(); },
@@ -15,12 +11,11 @@ try
 	);
 
 	// Plug-in execution block
-	$sqlStr = "SELECT COUNT(*), MIN(executed_at) FROM executed_plugin_list WHERE status = ?";
-	$result = DBConnector::query($sqlStr, Job::JOB_SUCCEEDED, 'ARRAY_NUM');
-	$executionNum = $result[0];
-	$oldestExecDate = substr($result[1], 0, 10);
+	$sqlStr = "SELECT COUNT(*) AS count, to_char(MIN(executed_at), 'YYYY-MM-DD')
+		AS first FROM executed_plugin_list WHERE status = ?";
+	$execStats = DBConnector::query($sqlStr, Job::JOB_SUCCEEDED, 'ARRAY_ASSOC');
 
-	if($executionNum > 0)
+	if($execStats['count'] > 0)
 	{
 		$sqlStr = "SELECT pm.plugin_name, pm.version, COUNT(el.job_id) as cnt"
 				. " FROM executed_plugin_list el, plugin_master pm"
@@ -32,181 +27,69 @@ try
 	// Message board
 	$topMessage = ServerParam::getVal('top_message');
 
-	//--------------------------------------------------------------------------
-	//// For latest missed TP
-	//--------------------------------------------------------------------------
-	$latestHtml = "";
-
-	if($_SESSION['personalFBFlg']==1 && $_SESSION['showMissed']!='none')
+	// latest missed TP
+	$user = Auth::currentUser();
+	Patient::$anonymizeMode = $user->needsAnonymization();
+	if ($user->hasPrivilege(Auth::PERSONAL_FEEDBACK_ENTER) && $user->show_missed != 'none')
 	{
-		$sqlStr = "SELECT el.job_id, el.plugin_id, cc.candidate_id "
-				. " FROM executed_plugin_list el, feedback_list fl, candidate_classification cc"
-				. " WHERE el.job_id=fl.job_id AND fl.fb_id = cc.fb_id";
+		$own = $user->show_missed == 'own' ? $own = "AND fb.entered_by = ?" : '';
+		$missed_jobs = DBConnector::query(
+			"SELECT el.job_id, cc.candidate_id AS display_id
+			FROM executed_plugin_list el
+				INNER JOIN feedback_list fb ON el.job_id = fb.job_id
+				INNER JOIN candidate_classification cc ON fb.fb_id = cc.fb_id
+			WHERE fb.is_consensual = 'f' AND cc.evaluation = 2 $own
+			ORDER BY fb.registered_at DESC
+			LIMIT 3",
+			$own ? array($user->user_id) : array(),
+			'ALL_ASSOC'
+		);
 
-		if($_SESSION['showMissed']=='own')  $sqlStr .= " AND fl.entered_by=?";
-
-		$sqlStr .= " AND fl.is_consensual='f'"
-				.  " AND cc.evaluation = 2"
-				.  " ORDER BY fl.registered_at DESC LIMIT 3;";
-
-		$stmt = $pdo->prepare($sqlStr);
-		if($_SESSION['showMissed']=='own')  $stmt->bindValue(1, $_SESSION['userID']);
-		$stmt->execute();
-
-		if($stmt->rowCount() > 0)
-		{
-			$idList = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-			foreach($idList as $idSet)
-			{
-				$sqlStr = "SELECT pm.plugin_name, pm.version, cm.result_table"
-						. " FROM plugin_master pm, plugin_cad_master cm"
-						. " WHERE pm.plugin_id=? AND cm.plugin_id=pm.plugin_id";
-				$pluginParams = DBConnector::query($sqlStr, $idSet['plugin_id'], 'ARRAY_ASSOC');
-
-				$sqlStr = "SELECT pt.patient_id, pt.patient_name, st.study_date, st.study_time,"
-						. " sr.study_instance_uid, sr.series_instance_uid, sm.storage_id, sm.path"
-						. " FROM patient_list pt, study_list st, series_list sr,"
-						. " storage_master sm, executed_series_list es"
-						. " WHERE es.job_id=? AND es.volume_id=0 AND sr.sid=es.series_sid"
-						. " AND st.study_instance_uid = sr.study_instance_uid"
-						. " AND pt.patient_id=st.patient_id"
-						. " AND sm.storage_id=sr.storage_id";
-				$seriesParams = DBConnector::query($sqlStr, $idSet['job_id'], 'ARRAY_ASSOC');
-
-				$sqlStr = 'SELECT location_x as x, location_y as y, location_z as z'
-						. ' FROM "' . $pluginParams['result_table'] . '"'
-						. ' WHERE job_id=? AND sub_id=?';
-				$candPos = DBConnector::query($sqlStr, array($idSet['job_id'], $idSet['candidate_id']), 'ARRAY_ASSOC');
-
-				$sqlStr = "SELECT key, value FROM executed_plugin_attributes WHERE job_id=?";
-				$stmt = $pdo->prepare($sqlStr);
-				$stmt->bindValue(1, $idSet['job_id']);
-				$stmt->execute();
-				$attributes = array();
-
-				foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $item)
-				{
-					$attributes[$item['key']] = $item['value'];
-				}
-
-				// Set parameters
-				$seriesDir = $seriesParams['path'] . $DIR_SEPARATOR . $seriesParams['patient_id']
-						   . $DIR_SEPARATOR . $seriesParams['study_instance_uid']
-				           . $DIR_SEPARATOR . $seriesParams['series_instance_uid'];
-
-				$baseName = sprintf('%s_%d_%d_%d_0_0.jpg',
-									$seriesParams['series_instance_uid'],
-									$candPos['z'],
-									$attributes['window_level'],
-									$attributes['window_width']);
-
-				$dumpFname = sprintf("%s_%d.txt", $seriesParams['series_instance_uid'], $candPos['z']);
-
-				// Get path of cache
-				$sqlStr = "SELECT storage_id, path FROM storage_master WHERE type=3 AND current_use='t'";
-				$result =  DBConnector::query($sqlStr, NULL, 'ARRAY_NUM');
-
-				$pathOfCache = $result[1] . $DIR_SEPARATOR;
-				$webPathOfCache = 'storage/' . $result[0] . '/';
-
-				$dstFname    = $pathOfCache . $baseName;
-				$dstFnameWeb = $webPathOfCache . $baseName;
-
-				if(!is_file($dstFname))
-				{
-					$srcFname = sprintf("%s%s%08d.dcm", $seriesDir, $DIR_SEPARATOR, $candPos['z']);
-					$dumpFname = $pathOfCache . $dumpFname;
-
-					$dcmResult = DcmExport::createThumbnailJpg(
-									$srcFname,
-									$dstFname,
-									$dumpFname,
-									100,
-									$attributes['window_level'],
-									$attributes['window_width'],
-									0, 0);
-				}
-
-				$dispWidth = 256;
-				$dispHeight = (int)((real)$attributes['crop_height'] * ($dispWidth / (real)$attributes['crop_width']) + 0.5);
-				$scale = $dispWidth/(real)$attributes['crop_width'];
-
-				$img = @imagecreatefromjpeg($dstFname);
-
-			    if($img)
-				{
-					$width  = imagesx($img);
-					$height = imagesy($img);
-					imagedestroy($img);
-				}
-
-				// Create HTML statement
-				$latestHtml .= '<div class="result-record-3cols al-c">'
-							.  '<div class="al-l" style="font-size:12px;">';
-
-				if($_SESSION['anonymizeFlg'] == 1)
-				{
-					$latestHtml .= '<b>&nbsp;Pt.: </b>' . PinfoScramble::scramblePtName()
-								.  ' ('
-								.  PinfoScramble::encrypt($seriesParams['patient_id'], $_SESSION['key'])
-								.  ')<br/>';
-				}
-				else
-				{
-					$latestHtml .= '<b>&nbsp;Pt.: </b>'
-								.  $seriesParams['patient_name']
-								. ' (' . $seriesParams['patient_id'] . ')<br/>';
-				}
-
-				$latestHtml .= '<b>&nbsp;St.: </b>' . $seriesParams['study_date']
-							.  '&nbsp;' . $seriesParams['study_time'] . '<br/>'
-							.  '<b>&nbsp;CAD: </b>' . $pluginParams['plugin_name'] . ' v.' . $pluginParams['version']
-							.  '<input name="" type="button" value="detail" class="form-btn"'
-							.  ' onclick="location.href=\'cad_results/cad_result.php?jobID=' . $idSet['job_id']
-							.  '&feedbackMode=personal&remarkCand=' . $idSet['candidate_id'] . '&sortKey=confidence'
-							.  '&sortOrder=DESC\';" style="margin-left:70px;"><br/>'
-							.  '<b>&nbsp;Candidate ID: </b>' . $idSet['candidate_id'] . '<br/>'
-							.  '</div>'
-							.  '<div style="width:' . $dispWidth . 'px; height:' .  $dispHeight . 'px;'
-							.  ' overflow:hidden; position:relative; margin-bottom:7px;" class="block-al-c">'
-							.  '<img class="transparent" src="cad_results/images/magenta_circle.png"'
-							.  ' style="position:absolute;'
-							.  ' left:' .  (($candPos['x']-(int)$attributes['crop_org_x'])*$scale-12) . 'px;'
-							.  ' top:' .(($candPos['y']-(int)$attributes['crop_org_y'])*$scale-12) . 'px; z-index:2;">'
-							.  '<img src="' . $dstFnameWeb . '" width=' . $width*$scale . ' height=' . $height*$scale
-							.  ' style="position:absolute; left:' . (-(int)$attributes['crop_org_x']*$scale) . 'px;'
-							.  ' top:' . (-(int)$attributes['crop_org_y']*$scale) . 'px;'
-							.  ' z-index:1;">'
-							.  '</div>'
-							.  '</div>';
+		$recentMissed = array();
+		foreach ($missed_jobs as $item) {
+			$job = new CadResult($item['job_id']);
+			$prn = $job->Plugin->presentation();
+			$dp = $prn->displayPresenter();
+			if (!($dp instanceof LesionCandDisplayPresenter))
+				break;
+			if (!($prn->feedbackListener() instanceof SelectionFeedbackListener))
+				break;
+			$displays = $job->getDisplays();
+			$display = $displays[$item['display_id']];
+			$crop = null;
+			$attr = $job->getAttributes();
+			if (isset($attr['crop_org_x']) && isset($attr['crop_org_y']) &&
+				isset($attr['crop_width']) && isset($attr['crop_height'])) {
+				$crop = array(
+					'x' => (int)$attr['crop_org_x'],
+					'y' => (int)$attr['crop_org_y'],
+					'width' => (int)$attr['crop_width'],
+					'height' => (int)$attr['crop_height'],
+				);
 			}
+			$opt = array();
+			if (isset($attr['window_level'])) $opt['wl'] = (float)$attr['window_level'];
+			if (isset($attr['window_width'])) $opt['ww'] = (float)$attr['window_width'];
+			$recentMissed[] = array(
+				'job' => $job,
+				'display' => $display,
+				'crop' => $crop,
+				'opt' => $opt
+			);
 		}
 	}
-	//--------------------------------------------------------------------------
 
-	//--------------------------------------------------------------------------
-	// Settings for Smarty
-	//--------------------------------------------------------------------------
 	$smarty = new SmartyEx();
-
 	$smarty->assign(array(
 		'plugins' => $plugins,
-		'executionNum' => $executionNum,
-		'oldestExecDate' => $oldestExecDate,
+		'execStats' => $execStats,
 		'cadExecutionData' => $cadExecutionData,
 		'topMessage' => $topMessage,
-		'latestHtml' => $latestHtml
+		'recentMissed' => $recentMissed
 	));
-
 	$smarty->display('home.tpl');
-	//--------------------------------------------------------------------------
-
 }
 catch (PDOException $e)
 {
 	var_dump($e->getMessage());
 }
-
-$pdo = null;
-
